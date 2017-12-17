afccc3914f66cced304b035b7a2370f640472fdb
Harry Markley <tremarkley@gmail.com>
2017-12-17 11:18:12 -0800

    Implement filter

--- src/underbar.js
+++ src/underbar.js
@@ -81,6 +81,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item, index) {
+      if (test(item)) {
+        result.push(item);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
