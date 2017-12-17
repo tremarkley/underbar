72763362b119302f8079021e12a5a85a345d5457
Harry Markley <tremarkley@gmail.com>
2017-12-17 11:31:20 -0800

    Implement reject

--- src/underbar.js
+++ src/underbar.js
@@ -95,6 +95,14 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    _.each(collection, function(item, index) {
+      if (!test(item)) {
+        result.push(item);
+      }
+    });
+
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
