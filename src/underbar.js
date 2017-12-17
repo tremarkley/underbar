5ec7ea531fbb233b90372c7976eff5ff44643e32
Harry Markley <tremarkley@gmail.com>
2017-12-17 11:02:45 -0800

    Implement last

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,10 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n > array.length) {
+      return array;
+    }
+    return n === undefined ? array[array.length - 1] : array.slice(array.length - n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
